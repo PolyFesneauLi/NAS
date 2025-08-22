3a0e63df3cf9f2927458bc1e43ad73c60460d4ee
PolyFesneauLi <24063117g@connect.polyu.hk>
2025-08-22 14:15:48 +0800

    JWT_EXPIRES_IN = 2h    idle 24000

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ module.exports = {
   DEFAULT_STORAGE_QUOTA: parseInt(process.env.DEFAULT_STORAGE_QUOTA) || 1024 * 1024 * 1024 * 500,
   
   // JWT配置
-  JWT_EXPIRATION: process.env.JWT_EXPIRES_IN || '1h', // 1小时 后JWT token 过期
+  JWT_EXPIRATION: process.env.JWT_EXPIRES_IN || '2h', // 1小时 后JWT token 过期
   jwtSecret: process.env.JWT_SECRET || 'nas_secret_key', // JWT密钥
   
   // 上传文件配置
